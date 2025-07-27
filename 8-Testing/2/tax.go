a7b05da6700b2c9dd5bb5e92aed5ae0a5fef253c
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-07-27 20:31:04 -0300

    importing testify lib to assert tests

---
+++ 8-Testing/2/tax.go
@@ -0,0 +1,17 @@
+package tax
+
+import (
+	"errors"
+	"time"
+)
+
+func CalculateTax(amount float64) (float64, error) {
+	time.Sleep(time.Millisecond)
+	if amount <= 0 {
+		return 0.0, errors.New("amount must be greater than 0")
+	}
+	if amount >= 1000 {
+		return 10.0, nil
+	}
+	return 5.0, nil
+}
