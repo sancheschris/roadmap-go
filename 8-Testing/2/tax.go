385f8df29b7bdd9b30c8c3650087c68187280757
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-07-28 21:23:10 -0300

    hands-on practice with mocking examples using testify mock

--- 8-Testing/2/tax.go
+++ 8-Testing/2/tax.go
@@ -5,7 +5,16 @@ import (
 	"time"
 )
 
-func CalculateTax(amount float64) (float64, error) {
+type Repository interface {
+	SaveTax(amount float64) error
+}
+
+func CalculateTaxAndSave(amount float64, repository Repository) error {
+	tax := CalculateTax(amount)
+	return repository.SaveTax(tax)
+}
+
+func CalculateTax2(amount float64) (float64, error) {
 	time.Sleep(time.Millisecond)
 	if amount <= 0 {
 		return 0.0, errors.New("amount must be greater than 0")
@@ -14,4 +23,15 @@ func CalculateTax(amount float64) (float64, error) {
 		return 10.0, nil
 	}
 	return 5.0, nil
+}
+
+func CalculateTax(amount float64) float64 {
+	time.Sleep(time.Millisecond)
+	if amount <= 0 {
+		return 0.0
+	}
+	if amount >= 1000 {
+		return 10.0
+	}
+	return 5.0
 }
