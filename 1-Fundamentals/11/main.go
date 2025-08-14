5b79bb4ec75a94c0ef0d2043844de7bfcd8fa38e
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-08-13 22:18:23 -0300

    hands on interfaces

--- 1-Fundamentals/11/main.go
+++ 1-Fundamentals/11/main.go
@@ -15,9 +15,26 @@ type Client struct {
 	Address
 }
 
+type Person interface {
+	Disable()
+}
+
+type Company struct {
+	Name string
+}
+
+func (c Company) Disable() {
+	c.Name = "Company XPTO"
+	fmt.Println(c.Name)
+}
+
 func (c Client) Disable() {
 	c.Active = false
 	fmt.Printf("The client %s was disabled\n", c.Name)
+}
+
+func Deactivated(person Person) {
+	person.Disable()
 }
 
 func main() {
@@ -28,7 +45,13 @@ func main() {
 	}
 
 	chris.City = "SP"
-	chris.Disable()
 
-	fmt.Printf("The client name is %s, their age: %d, and their status is: %t and city %s", chris.Name, chris.Age, chris.Active, chris.City)
+	company := Company{
+		"YPTO",
+	}
+	
+	Deactivated(chris)
+	Deactivated(company)
+
+	// fmt.Printf("The client name is %s, their age: %d, and their status is: %t and city %s", chris.Name, chris.Age, chris.Active, chris.City)
 }
