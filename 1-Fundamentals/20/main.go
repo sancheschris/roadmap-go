eb9ee990650f5dfc2be277a6f394ff792a8c8c9f
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-08-15 21:47:21 -0300

    switch case

---
+++ 1-Fundamentals/20/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	a := 1
+	// b := 2
+	// c := 3
+
+	switch a {
+	case 1:
+		println("a")
+	case 2:
+		println("b")
+	case 3: 
+		println("c")
+	default:	
+	println("d")
+	}
+
+}
