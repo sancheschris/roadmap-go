447f3112a921ef7687380066ca7a9b77c437209d
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-08-15 21:27:05 -0300

    how to export methods, variable and add them as private in go

--- 1-Fundamentals/18/main.go
+++ 1-Fundamentals/18/main.go
@@ -9,5 +9,8 @@ import (
 func main() {
 	s := matematica.Soma(10, 20)
 
+	// result := matematica.dividir(10.5, 2.5) 
+
 	fmt.Printf("Resultado: %v", s)
+	fmt.Println(result)
 }
--- 1-Fundamentals/18/matematica/matematica.go
+++ 1-Fundamentals/18/matematica/matematica.go
@@ -3,3 +3,11 @@ package matematica
 func Soma[T int | float64](a, b T) T {
 	return a + b
 }
+
+// this is not exported - you can only use it inside this package (use capital letter to export)
+func dividir(a, b float64) float64 {
+	if (b > 0) {
+		return a / b
+	}
+	return 0.0
+}
